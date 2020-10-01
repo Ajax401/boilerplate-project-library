e118b0977a7ee1a8f33e24c66b04f72def4babeb
Anton Pretorius <Ajax401@users.noreply.github.com>
2020-10-01 09:28:25 +0200

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,10 +9,10 @@
 var chaiHttp = require('chai-http');
 var chai = require('chai');
 var assert = chai.assert;
+let shortId = require('shortid');
 var server = require('../server');
 
 chai.use(chaiHttp);
-
 suite('Functional Tests', function() {
 
   /*
@@ -35,17 +35,37 @@ suite('Functional Tests', function() {
   * ----[END of EXAMPLE TEST]----
   */
 
-  suite('Routing tests', function() {
+   suite('Routing tests', function() {
 
-
+    let id;
     suite('POST /api/books with title => create book object/expect book object', function() {
       
       test('Test POST /api/books with title', function(done) {
-        //done();
+         chai.request(server)
+          .post('/api/books')
+          .send({
+            title: 'Sum of All Fears'
+          })
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.equal(res.body.title, 'Sum of All Fears')
+            assert.isNotNull(res.body._id)//res.body._id has an id value
+            id = res.body._id;
+            console.log("id has been set as " + id)
+            done();
+          });
       });
       
       test('Test POST /api/books with no title given', function(done) {
-        //done();
+      chai.request(server)
+          .post('/api/books')
+          .send({})
+          .end(function(err, res){
+            console.log(res.body)
+            assert.equal(res.status,200)
+            assert.equal(res.body, 'No title in input field')
+            done();
+          });
       });
       
     });
@@ -54,20 +74,45 @@ suite('Functional Tests', function() {
     suite('GET /api/books => array of books', function(){
       
       test('Test GET /api/books',  function(done){
-        //done();
+        chai.request(server)
+          .get('/api/books')
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.isArray(res.body, 'Should contain an Array')
+            assert.property(res.body[0], 'commentcount','Should have number count')
+            assert.property(res.body[0], 'title','Should be a valid String title')
+            assert.property(res.body[0], '_id','Should contain random number and String mix')
+            done();
+          });
       });      
       
     });
 
 
     suite('GET /api/books/[id] => book object with [id]', function(){
-      
+      let myId = shortId.isValid('bla bla')
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        //done();
+         chai.request(server)
+         .get('/api/books/' + myId)
+         .end(function(err,res){
+           console.log(myId)
+           assert.equal(res.status,200)
+           console.log(res.body)
+           assert.strictEqual(res.body,'no book exists','Is a invalid Id')
+           done()
+         })
+        
       });
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+           chai.request(server)
+          .get('/api/books/' + id)
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.equal(res.body._id, id)
+            assert.equal(res.body.title, 'Sum of All Fears')
+						done()
+          });
       });
       
     });
@@ -76,7 +121,25 @@ suite('Functional Tests', function() {
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+           chai.request(server)
+          .post('/api/books/' + id)
+          .send({
+            comment: 'test comment'
+          })
+          .end(function(err, res){
+
+            assert.isTrue(res.body.comments.includes('test comment'))
+
+						chai
+						.request(server)
+						.delete("/api/books/" + id)
+						.send({})
+						.end(function(err, res) {
+							done();
+						});
+          });
+
+
       });
       
     });
